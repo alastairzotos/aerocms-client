6eb1a5e3851c4704ba06ed3ccf916690ebf5f4c4
Alastair <alastairzotos@gmailcom>
2020-11-15 18:43:47 +0000

    test no loadable

--- src/modules/auth/pages/index.ts
+++ src/modules/auth/pages/index.ts
@@ -1,6 +1,9 @@
 import loadable from '@loadable/component';
 import { Pages } from '~/core';
 
+import LoginPage from './LoginPage';
+
 export const pages: Pages = {
-    '/aero-login': loadable(async () => import('./LoginPage'))
+    // '/aero-login': loadable(async () => import('./LoginPage'))
+    '/aero-login': LoginPage
 };
